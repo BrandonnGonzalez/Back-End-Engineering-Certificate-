83b56a1d80fe56b1de264deb1446d322b7ef26bd
Brandon Gonzalez <120545284+BrandonnGonzalez@users.noreply.github.com>
2024-10-29 20:45:17 -0400

    Create DogYears.js

---
+++ JavaScript/Projects/DogYears.js
@@ -0,0 +1,22 @@
+/*
+Dogs mature at a faster rate than human beings. We often say a dog’s age can be calculated in “dog years” to account for their growth compared to a human of the same age. In some ways we could say, time moves quickly for dogs — 8 years in a 
+human’s life equates to 45 years in a dog’s life. 
+How old would you be if you were a dog?
+*/
+
+const myAge = 21;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+laterYears = myAge - 2
+laterYears *= 4;
+console.log(earlyYears)
+console.log(laterYears)
+
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Brandon'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${laterYears} years old in dog years.`);
+// Output: "My name is brandon. I am 21 years old in human years which is 76 years old in dog years."
+
+ 
