9c64f646a6e5e5c46f14db3fa8776b48a2cd3988
Brandon Gonzalez <120545284+BrandonnGonzalez@users.noreply.github.com>
2024-11-08 19:45:35 -0500

    Update MealMaker.js

--- JavaScript/JavaScript II/Objects/Projects/MealMaker.js
+++ JavaScript/JavaScript II/Objects/Projects/MealMaker.js
@@ -1,3 +1,8 @@
+/*
+ A restaurant has hired you to create a function for their website that allows them to set a meal and price each morning for Today’s Special. 
+ Use your knowledge of getters and setters to make sure anyone who uses the new function can generate a meal and a price for Today’s Special without any embarrassing errors!
+*/
+
 // step 1: Object creation
 const menu = {
   // step 2 and 3: Property creation
