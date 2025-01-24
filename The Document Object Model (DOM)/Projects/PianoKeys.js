2210db0485058899f08f5d29e4ef6c3e23ce4e09
Brandon Gonzalez <120545284+BrandonnGonzalez@users.noreply.github.com>
2025-01-23 23:53:48 -0500

    Update PianoKeys.js

--- The Document Object Model (DOM)/Projects/PianoKeys.js
+++ The Document Object Model (DOM)/Projects/PianoKeys.js
@@ -5,7 +5,7 @@ students study! With this role, I created a web piano player with DOM events usi
 
 
 
-// The keys and notes variables store the piano keys
+// The keys and notes variables store the piano keys.
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
 keys.forEach(function(key){
